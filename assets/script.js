ba798d53badc8e65f0bbd58d756845245f7b5c76
Derick Stadler <derickstadler@gmail.com>
2023-06-01 13:22:33 -0600

    updated html js css with map functionality and nav, deleted cards and potential card boxs derick

--- assets/script.js
+++ assets/script.js
@@ -21,6 +21,24 @@ themeSwitcher.addEventListener("click", function () {
   }
 });
 
+// map functionality.
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZHNzdGFkMDIiLCJhIjoiY2xpYnl1b3VjMGZ0ZDNwbjFxbmR3ejdqcSJ9.3mwOKhxYibQ9YZdqNZHErQ";
+const map = new mapboxgl.Map({
+  container: "map",
+  // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+  style: "mapbox://styles/mapbox/streets-v12",
+  center: [-104.9903, 39.7392],
+  zoom: 12,
+});
+
+map.addControl(
+  new MapboxDirections({
+    accessToken: mapboxgl.accessToken,
+  }),
+  "top-left"
+);
+
 var cityStart = document.getElementById("cityStart");
 var cityEnd = document.getElementById("cityEnd");
 var searchButton = document.getElementById("search");
