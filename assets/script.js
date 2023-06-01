f7eb14e73b57cf60c2c436871ca9fb43af1b58d6
unknown <john.gordos@outlook.com>
2023-06-01 13:53:17 -0600

    added baseline map functionality, some cleanup

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,69 @@ var themeSwitcher = document.querySelector("#theme-switcher");
 var container = document.querySelector(".container");
 var header = document.querySelector("#header");
 var footer = document.querySelector("#footer");
+var cityStart = document.getElementById("cityStart");
+var cityEnd = document.getElementById("cityEnd");
+var searchButton = document.getElementById("search");
 
 var mode = "dark";
+
+// map functionality.
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZHNzdGFkMDIiLCJhIjoiY2xpYnl1b3VjMGZ0ZDNwbjFxbmR3ejdqcSJ9.3mwOKhxYibQ9YZdqNZHErQ";
+const map = new mapboxgl.Map({
+  container: "map",
+  // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+  style: "mapbox://styles/mapbox/streets-v12",
+  center: [-104.9903, 39.7392],
+  zoom: 12,
+});
+
+var directions = new MapboxDirections({
+  accessToken: mapboxgl.accessToken,
+  unit: "imperial",
+  profile: "mapbox/driving",
+});
+
+map.addControl(directions, "top-right");
+
+function getCoords(event) {
+  console.log("ROUTE EVENT!");
+  var steps = $(".mapbox-directions-step");
+  var coordinates = [];
+  for (var i = 0; i < steps.length; i++) {
+    console.log(steps[i]);
+    var coords = [steps[i].dataset.lng, steps[i].dataset.lat];
+    coordinates.push(coords);
+  }
+  //! MOVE STEPS TO OTHER CONTAINER
+  console.log(coordinates);
+  // ?Sample coordinates
+  //weather api
+
+  //marker build
+
+  // Add markers to the map.
+  for (var j = 0; j < coordinates.length; j++) {
+    console.log("woot array");
+    console.log(coordinates[j]);
+    const el = document.createElement("div");
+    const width = 40;
+    const height = 40;
+    el.style.backgroundImage = `url(https://placekitten.com/g/${width}/${height}/)`; //images local; dependent on weather desc
+    el.style.width = `${width}px`;
+    el.style.height = `${height}px`;
+    el.style.backgroundSize = "100%";
+
+    el.addEventListener("click", () => {
+      window.alert(marker.properties.message);
+    });
+    new mapboxgl.Marker(el).setLngLat(coordinates[j]).addTo(map); //dynamically pass in coordinates  [-97.323982, 37.604087]
+  }
+
+  // approx location based on coords
+}
+
+directions.on("route", getCoords);
 
 themeSwitcher.addEventListener("click", function () {
   if (mode === "dark") {
@@ -20,39 +81,3 @@ themeSwitcher.addEventListener("click", function () {
     footer.setAttribute("class", "darkFooter");
   }
 });
-
-// map functionality.
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiZHNzdGFkMDIiLCJhIjoiY2xpYnl1b3VjMGZ0ZDNwbjFxbmR3ejdqcSJ9.3mwOKhxYibQ9YZdqNZHErQ";
-const map = new mapboxgl.Map({
-  container: "map",
-  // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-  style: "mapbox://styles/mapbox/streets-v12",
-  center: [-104.9903, 39.7392],
-  zoom: 12,
-});
-
-map.addControl(
-  new MapboxDirections({
-    accessToken: mapboxgl.accessToken,
-  }),
-  "top-left"
-);
-
-var cityStart = document.getElementById("cityStart");
-var cityEnd = document.getElementById("cityEnd");
-var searchButton = document.getElementById("search");
-
-searchButton.addEventListener("click", function (event) {
-  var savedTrips = {
-    cityStart: cityStart.value,
-    cityEnd: cityEnd.value,
-  };
-
-  this.localStorage.setItem("savedTrips", JSON.stringify(savedTrips));
-  displayTrip();
-});
-
-function displayTrip() {
-  // add function here
-}
