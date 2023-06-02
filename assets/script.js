dc8994d9ccbb5e39de8c43e006c752b74c9de082
unknown <john.gordos@outlook.com>
2023-06-02 13:59:25 -0600

    arbitrarily reducing points along route

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ var footer = $("#footer");
 
 var mode = "dark";
 var routeWeatherData = new Map();
-var latLonWeatherDataKeySeparator = ",";
+var latLonKeySeparator = ",";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZHNzdGFkMDIiLCJhIjoiY2xpYnl1b3VjMGZ0ZDNwbjFxbmR3ejdqcSJ9.3mwOKhxYibQ9YZdqNZHErQ";
@@ -25,7 +25,7 @@ var directions = new MapboxDirections({
 });
 
 map.addControl(directions, "top-right");
-mapTest();
+moveDirections();
 
 directions.on("route", handleRoute);
 
@@ -49,43 +49,83 @@ function handleRoute() {
   var markers = $("div[data-marker]");
   if (markers) markers.remove();
   var steps = $(".mapbox-directions-step");
-  var coordinates = [];
+  var routeDistance = $(".mapbox-directions-route-summary")[0].children[1]
+    .textContent;
+  var routeLength = routeDistance.substring(0, routeDistance.length - 2);
+  var avgLength = routeLength / steps.length;
+  var sumOfLengthDiff = 0;
+  var stepLengths = new Map();
   for (var i = 0; i < steps.length; i++) {
-    //? filter coords?
-    coordinates.push([steps[i].dataset.lat, steps[i].dataset.lng]);
+    if (steps[i].children[2]) {
+      var stepLat = steps[i].dataset.lat;
+      var stepLng = steps[i].dataset.lng;
+      var stepDistance = steps[i].children[2].textContent.trim();
+      var stepLength = Number.parseFloat(
+        stepDistance.substring(0, stepDistance.length - 2)
+      );
+      var stepUnit = stepDistance.substring(stepDistance.length - 2);
+      if (stepUnit === "mi") {
+        sumOfLengthDiff += Math.pow(stepLength - avgLength, 2);
+        stepLengths.set(stepLat + latLonKeySeparator + stepLng, stepLength);
+      } else if (stepUnit === "ft") {
+        var stepLengthMi = stepLength / 5280;
+        sumOfLengthDiff += Math.pow(stepLengthMi - avgLength, 2);
+        stepLengths.set(stepLat + latLonKeySeparator + stepLng, stepLengthMi);
+      }
+    }
+    //ONLY PUSH RELEVANT POINTS - move out of this loop
+    //coordinates.push([stepLat, stepLng]);
   }
+  var coordinates = [];
+  var stDevLength = Math.pow(sumOfLengthDiff / steps.length, 0.5);
+  for (var latLng of stepLengths.keys()) {
+    console.log(stepLengths.get(latLng));
+    if (stepLengths.get(latLng) > stDevLength) {
+      var latLngArr = latLng.split(latLonKeySeparator);
+      coordinates.push([latLngArr[0], latLngArr[1]]);
+    }
+  }
+  //rethink adding origin back; may have clustering at start and end
+  console.log("all 'safe coords'");
+  console.log(coordinates);
+  var originLngLatArr = directions.getOrigin().geometry.coordinates;
+  var destLngLatArr = directions.getDestination().geometry.coordinates;
+  coordinates.push([originLngLatArr[1], originLngLatArr[0]]);
+  coordinates.push([destLngLatArr[1], destLngLatArr[0]]);
   for (var j = 0; j < coordinates.length; j++) {
     retrieveWeatherFromLocation(coordinates[j][0], coordinates[j][1]);
   }
 }
 function retrieveWeatherFromLocation(lat, lon) {
   fetch(
-    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+    "https://api.openweathermap.org/data/3.0/onecall?lat=" +
       lat +
       "&lon=" +
       lon +
-      "&appid=5a5f2543215b0ae09a5dc07887c20551&units=imperial"
+      "&appid=212be1e5713240df908c291b8fbba3f8&units=imperial"
   )
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       var weatherToday = data.list[0];
-      var el = $("<div>");
-      el.css(
+      var markerEl = $("<div>");
+      markerEl.css(
         "backgroundImage",
         'url("https://openweathermap.org/img/wn/' +
           weatherToday.weather[0].icon.replace("n", "d") +
           '@2x.png")'
       );
-      el.attr("data-marker", "true");
-      el.css("width", 40);
-      el.css("height", 40);
-      el.css("backgroundSize", "100%");
-      el.attr("data-lat", lat);
-      el.attr("data-lon", lon);
+      markerEl.attr("data-marker", "true");
+      markerEl.css("width", 40);
+      markerEl.css("height", 40);
+      markerEl.css("backgroundSize", "100%");
+      markerEl.attr("data-lat", lat);
+      markerEl.attr("data-lon", lon);
+      markerEl.attr("data-toggle", "modal");
+      markerEl.attr("data-target", "#exampleModal");
       routeWeatherData.set(
-        lat + latLonWeatherDataKeySeparator + lon,
+        lat + latLonKeySeparator + lon,
         new WeatherData(
           weatherToday.main.humidity,
           weatherToday.main.temp,
@@ -95,11 +135,13 @@ function retrieveWeatherFromLocation(lat, lon) {
         )
       );
 
-      el.on("click", placeholderModalCall);
+      markerEl.on("click", placeholderModalCall);
       // Add markers to the map.
       var tweakedLon = Number.parseFloat(lon + 0.003);
       var tweakedLat = Number.parseFloat(lat + 0.003);
-      new mapboxgl.Marker(el[0]).setLngLat([tweakedLon, tweakedLat]).addTo(map);
+      new mapboxgl.Marker(markerEl[0])
+        .setLngLat([tweakedLon, tweakedLat])
+        .addTo(map);
     })
     .catch(function (error) {
       console.log(error);
@@ -108,9 +150,7 @@ function retrieveWeatherFromLocation(lat, lon) {
 function placeholderModalCall(event) {
   event.preventDefault();
   var weatherData = routeWeatherData.get(
-    event.target.dataset.lat +
-      latLonWeatherDataKeySeparator +
-      event.target.dataset.lon
+    event.target.dataset.lat + latLonKeySeparator + event.target.dataset.lon
   );
   alert(
     "Humidity: " +
@@ -137,10 +177,9 @@ function WeatherData(humidity, temp, visibility, description, windGust) {
 themeSwitcher.on("click", switchTheme);
 
 // Justins work
-function mapTest() {
+function moveDirections() {
   const output = $("#mapTest");
   output.empty();
   const input = $(".mapboxgl-ctrl");
   output.append(input[0]);
-  console.log(input);
 }
