42667a6ce415cd4862b9a0e66ad0141ad2645248
unknown <john.gordos@outlook.com>
2023-06-02 09:48:40 -0600

    code cleanup, updated icons (bye kitties), and built a simple alert for weather data

--- assets/script.js
+++ assets/script.js
@@ -4,13 +4,12 @@ var header = $("#header");
 var footer = $("#footer");
 
 var mode = "dark";
+var routeWeatherData = new Map();
 
-// map functionality.
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZHNzdGFkMDIiLCJhIjoiY2xpYnl1b3VjMGZ0ZDNwbjFxbmR3ejdqcSJ9.3mwOKhxYibQ9YZdqNZHErQ";
 const map = new mapboxgl.Map({
   container: "map",
-  // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
   style: "mapbox://styles/mapbox/streets-v12",
   center: [-104.9903, 39.7392],
   zoom: 12,
@@ -26,84 +25,9 @@ var directions = new MapboxDirections({
 
 map.addControl(directions, "top-right");
 
-function getCoords(event) {
-  var steps = $(".mapbox-directions-step");
-  var coordinates = [];
-  for (var i = 0; i < steps.length; i++) {
-    //filter coords
-    coordinates.push([steps[i].dataset.lat, steps[i].dataset.lng]);
-  }
-  console.log(coordinates);
-  var stepContainer = $(".directions-control-instructions");
-  $("#route-instructions-container").append(stepContainer);
-  for (var j = 0; j < coordinates.length; j++) {
-    /*
-    weather api
-
-    delay until weather retrieved for all coords
-    */
-    retrieveWeatherFromLocation(coordinates[j][0], coordinates[j][1]);
-  }
-}
-function retrieveWeatherFromLocation(lat, lon) {
-  //
-  //data/3.0/onecall?
-  fetch(
-    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
-      lat +
-      "&lon=" +
-      lon +
-      "&appid=5a5f2543215b0ae09a5dc07887c20551&units=imperial"
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      const el = document.createElement("div"); //JQUERY THIS AND DEPENDENCIES
-      const width = 40;
-      const height = 40;
-      el.style.backgroundImage = `url(https://placekitten.com/g/${width}/${height}/)`; //images local; dependent on weather desc
-      el.style.width = `${width}px`;
-      el.style.height = `${height}px`;
-      el.style.backgroundSize = "100%";
-      el.setAttribute("data-lat", lat);
-      el.setAttribute("data-lon", lon);
-      allWeatherData.set(
-        lat + "-" + lon,
-        new WeatherData(
-          data.list[0].main.humidity,
-          data.list[0].main.temp,
-          data.list[0].visibility,
-          data.list[0].weather[0].description,
-          data.list[0].wind.gust
-        )
-      );
-      el.addEventListener("click", placeholderModalCall);
-      // Add markers to the map.
-      var tweakedLon = Number.parseFloat(lon + 0.003);
-      var tweakedLat = Number.parseFloat(lat + 0.003);
-      console.log(typeof tweakedLat);
-      new mapboxgl.Marker(el).setLngLat([tweakedLon, tweakedLat]).addTo(map);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
-function placeholderModalCall(event) {
-  console.log(event.target);
-}
-// TEMP SPOT
-var allWeatherData = new Map(); //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map#instance_methods
-function WeatherData(humidity, temp, visibility, description, windGust) {
-  this.humidity = humidity;
-  this.temp = temp;
-  this.visibility = visibility;
-  this.description = description;
-  this.windGust = windGust;
-}
 directions.on("route", getCoords);
 
-themeSwitcher.on("click", function () {
+function switchTheme() {
   if (mode === "dark") {
     mode = "light";
     themeSwitcher.attr("class", "switchBox");
@@ -117,4 +41,91 @@ themeSwitcher.on("click", function () {
     header.attr("class", "darkHeader");
     footer.attr("class", "darkFooter");
   }
-});
+}
+
+function getCoords() {
+  var steps = $(".mapbox-directions-step");
+  var coordinates = [];
+  for (var i = 0; i < steps.length; i++) {
+    //? filter coords?
+    coordinates.push([steps[i].dataset.lat, steps[i].dataset.lng]);
+  }
+  var stepContainer = $(".directions-control-instructions");
+  $("#route-instructions-container").append(stepContainer);
+  for (var j = 0; j < coordinates.length; j++) {
+    retrieveWeatherFromLocation(coordinates[j][0], coordinates[j][1]);
+  }
+}
+function retrieveWeatherFromLocation(lat, lon) {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=5a5f2543215b0ae09a5dc07887c20551&units=imperial"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var el = $("<div>"); //JQUERY THIS AND DEPENDENCIES
+      el.css(
+        "backgroundImage",
+        'url("https://openweathermap.org/img/wn/' +
+          data.list[0].weather[0].icon.replace("n", "d") +
+          '@2x.png")'
+      );
+      el.css("width", 40);
+      el.css("height", 40);
+      el.css("backgroundSize", "100%");
+      el.attr("data-lat", lat);
+      el.attr("data-lon", lon);
+      routeWeatherData.set(
+        lat + "," + lon,
+        new WeatherData(
+          data.list[0].main.humidity,
+          data.list[0].main.temp,
+          data.list[0].visibility,
+          data.list[0].weather[0].description,
+          data.list[0].wind.gust
+        )
+      );
+
+      el.on("click", placeholderModalCall);
+      // Add markers to the map.
+      var tweakedLon = Number.parseFloat(lon + 0.003);
+      var tweakedLat = Number.parseFloat(lat + 0.003);
+      new mapboxgl.Marker(el[0]).setLngLat([tweakedLon, tweakedLat]).addTo(map);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+function placeholderModalCall(event) {
+  event.preventDefault();
+  var weatherData = routeWeatherData.get(
+    event.target.dataset.lat + "," + event.target.dataset.lon
+  );
+  alert(
+    "Humidity: " +
+      weatherData.humidity +
+      "\nTemp: " +
+      weatherData.temp +
+      "\nVisibility: " +
+      weatherData.visibility +
+      "\nDescription: " +
+      weatherData.description +
+      "\nWind: " +
+      weatherData.windGust
+  );
+}
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map#instance_methods
+function WeatherData(humidity, temp, visibility, description, windGust) {
+  this.humidity = humidity;
+  this.temp = temp;
+  this.visibility = visibility;
+  this.description = description;
+  this.windGust = windGust;
+}
+
+themeSwitcher.on("click", switchTheme);
