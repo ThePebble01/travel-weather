7676f8dae5777c501e97ef489fc1b6938111268b
unknown <john.gordos@outlook.com>
2023-09-05 10:31:15 -0600

    made use of async await, spearated marker build from retrieving weather data, updated variable names

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,16 @@
-//make use of async await
-
 var routeWeatherData = new Map();
 var latLonKeySeparator = ",";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZHNzdGFkMDIiLCJhIjoiY2xpYnl1b3VjMGZ0ZDNwbjFxbmR3ejdqcSJ9.3mwOKhxYibQ9YZdqNZHErQ";
+
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v12",
   center: [-104.9903, 39.7392],
   zoom: 12,
 });
+
 const directions = new MapboxDirections({
   accessToken: mapboxgl.accessToken,
   unit: "imperial",
@@ -18,6 +18,7 @@ const directions = new MapboxDirections({
   routePadding: 10,
   interactive: false,
 });
+
 map.addControl(directions, "top-right");
 moveDirectionsControl();
 directions.on("route", handleRoute);
@@ -29,7 +30,7 @@ function moveDirectionsControl() {
   routeInstructions.append(mapboxControls[0]);
 }
 
-function handleRoute() {
+async function handleRoute() {
   resetMarkers();
   var steps = $(".mapbox-directions-step");
   var routeDistance = $(".mapbox-directions-route-summary")[0].children[1]
@@ -44,14 +45,18 @@ function handleRoute() {
     routeCoordinates
   );
   for (var j = 0; j < routeCoordinates.length; j++) {
-    // break down function, consider changing param to an array
-    retrieveWeatherFromLocation(routeCoordinates[j][0], routeCoordinates[j][1]);
+    await retrieveWeatherFromLocation(
+      routeCoordinates[j][0],
+      routeCoordinates[j][1]
+    );
   }
 }
+
 function resetMarkers() {
   var markers = $("div[data-marker]");
   if (markers) markers.remove();
 }
+
 function organizeCoordsRespectingStandardDeviation(routeLength, steps) {
   var coordinateResults = [];
   var stepLengths = new Map();
@@ -95,6 +100,7 @@ function organizeCoordsRespectingStandardDeviation(routeLength, steps) {
   ]);
   return coordinateResults;
 }
+
 function appendCoordinatesForStepsWithLargeDistance(routeLength, coordinates) {
   var priorPoint;
   var stepDistances = [];
@@ -129,64 +135,62 @@ function appendCoordinatesForStepsWithLargeDistance(routeLength, coordinates) {
   }
   return coordinates;
 }
+
 function calculateEuclideanDistance(x1, y1, x2, y2) {
   return Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 0.5);
 }
-//review api documentation to see if this can be bulkified....
-function retrieveWeatherFromLocation(lat, lon) {
-  fetch(
-    "https://api.openweathermap.org/data/3.0/onecall?lat=" +
-      lat +
-      "&lon=" +
-      lon +
-      "&appid=b8fc387331c767a99a233c98e09002f5&units=imperial"
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      var weatherToday = data.current; // return this, pass data to buildMarker f()
-      var markerEl = $("<div>");
-      markerEl.css(
-        "backgroundImage",
-        'url("https://openweathermap.org/img/wn/' +
-          weatherToday.weather[0].icon.replace("n", "d") +
-          '@2x.png")'
-      );
-      markerEl.css({ width: 40, height: 40, backgroundSize: "100%" });
-      markerEl.attr({
-        "data-marker": "true",
-        "data-lat": lat,
-        "data-lon": lon,
-        "data-toggle": "modal",
-        "data-target": "#exampleModal",
-      });
-      routeWeatherData.set(
-        lat + latLonKeySeparator + lon,
-        new WeatherData(
-          data.current.temp,
-          weatherToday.weather[0].icon.replace("n", "d"),
-          weatherToday.weather[0].description,
-          data.daily[0].temp.min,
-          data.daily[0].temp.max
-        )
-      );
-      markerEl.on("click", handleMarkerModalOpen);
-      // Add markers to the map.
-      var tweakedLon = Number.parseFloat(lon + 0.003);
-      var tweakedLat = Number.parseFloat(lat + 0.003);
-      new mapboxgl.Marker(markerEl[0])
-        .setLngLat([tweakedLon, tweakedLat])
-        .addTo(map);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+
+async function retrieveWeatherFromLocation(lat, lon) {
+  try {
+    const response = await fetch(
+      "https://api.openweathermap.org/data/3.0/onecall?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&appid=b8fc387331c767a99a233c98e09002f5&units=imperial"
+    );
+    const data = await response.json();
+    buildMarkersForWeatherData(data, lat, lon);
+  } catch (error) {
+    console.log(error);
+  }
 }
-function handleModalClose(event) {
-  event.preventDefault();
-  $("#weatherModal").css("display", "none");
+
+function buildMarkersForWeatherData(weatherData, lat, lon) {
+  var markerEl = $("<div>");
+  markerEl.css(
+    "backgroundImage",
+    'url("https://openweathermap.org/img/wn/' +
+      weatherData.current.weather[0].icon.replace("n", "d") +
+      '@2x.png")'
+  );
+  markerEl.css({ width: 40, height: 40, backgroundSize: "100%" });
+  markerEl.attr({
+    "data-marker": "true",
+    "data-lat": lat,
+    "data-lon": lon,
+    "data-toggle": "modal",
+    "data-target": "#exampleModal",
+  });
+  routeWeatherData.set(
+    lat + latLonKeySeparator + lon,
+    new WeatherData(
+      weatherData.current.temp,
+      weatherData.current.weather[0].icon.replace("n", "d"),
+      weatherData.current.weather[0].description,
+      weatherData.daily[0].temp.min,
+      weatherData.daily[0].temp.max
+    )
+  );
+  markerEl.on("click", handleMarkerModalOpen);
+  // Add markers to the map.
+  var adjustedLon = Number.parseFloat(lon + 0.003);
+  var adjustedLat = Number.parseFloat(lat + 0.003);
+  new mapboxgl.Marker(markerEl[0])
+    .setLngLat([adjustedLon, adjustedLat])
+    .addTo(map);
 }
+
 function handleMarkerModalOpen(event) {
   $("#weatherModal").css("display", "block");
   event.preventDefault();
@@ -209,6 +213,12 @@ window.onclick = function (event) {
     event.target.style.display = "none";
   }
 };
+
+function handleModalClose(event) {
+  event.preventDefault();
+  $("#weatherModal").css("display", "none");
+}
+
 class WeatherData {
   constructor(temp, icon, description, min, max) {
     this.temp = temp;
@@ -218,12 +228,14 @@ class WeatherData {
     this.max = max;
   }
 }
+
 class SearchGeocodeResults {
   constructor(origin, destination) {
     this.origin = origin;
     this.destination = destination;
   }
 }
+
 class ReverseGeocodeResult {
   constructor(city, state, country, lat, lng) {
     this.city = city;
